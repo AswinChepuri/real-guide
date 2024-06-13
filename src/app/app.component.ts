ac9bc73d66cf15f614c031518379c79c39c4a055
Shashi Dumpati <shashi.dumpati@realpage.com>
2024-06-13 14:44:48 +0530

    v2 version changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import AOS from 'aos';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,10 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'real-guide';
+
+  ngOnInit() {
+    AOS.init();
+  }
 }
